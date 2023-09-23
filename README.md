d28bc748a04d2835661511e28d29b0dfaed5f926
cyfung1031 <44498510+cyfung1031@users.noreply.github.com>
2023-09-24 00:12:04 +0900

    VM-Repack-2154

---
+++ README.md
@@ -0,0 +1,71 @@
+### README.md
+
+```markdown
+# VM-Repack-2154
+
+VM-Repack-2154 is a simple command-line utility to edit a specific text file inside a zip file, make specified replacements, and repack it.
+
+## Installation
+
+To use this utility, you'll need to clone this repository to your local machine.
+
+```sh
+git clone https://github.com/cyfung1031/vm-repack-2154.git
+cd vm-repack-2154
+npm install
+npm link
+```
+
+This will install all necessary dependencies and link the project to your global npm modules, allowing you to run the `vm-repack-2154` command from anywhere.
+
+## Usage
+
+```sh
+vm-repack-2154 --file [path-to-zip-file] [options]
+```
+
+### Options
+
+- `--file, -f` (required): The path to the zip file you want to edit.
+- `--textFile, -t` (optional): The name of the text file to edit inside the zip file. Default is `options/index.js`.
+- `--suffix, -s` (optional): Suffix to append to the original file name if provided.
+- `--output, -o` (optional): Path to save the edited zip file. By default, it will override the original zip file.
+
+### Example
+
+```sh
+vm-repack-2154 --file /downloads/folder/Violentmonkey-beta-webext-v2.15.4.zip --suffix _modified
+```
+
+This will create a new zip file named `Violentmonkey-beta-webext-v2.15.4_modified.zip` in the same directory as the original zip file, with the specified text file edited as per the replacement rules defined.
+
+## Replacements
+
+The utility will make the following replacements in the specified text file inside the zip:
+
+1. Replace all occurrences of `.readOnly` with `.ReadOnly`
+2. Replace all occurrences of `readOnly` with `readOnlY`
+3. Replace all occurrences of `.canEdit=!1` with `.canEdit=!0`
+
+## Uninstallation
+
+To uninstall VM-Repack-2154, navigate to the project's root directory and run:
+
+```sh
+npm unlink vm-repack-2154
+```
+
+This will remove the global link to the package. After doing this, you can also delete the project folder if you no longer need it.
+
+```sh
+cd ..
+rm -r vm-repack-2154
+```
+
+## Contributions
+
+Feel free to fork the project, open a pull request, or report any issues or feature requests in the GitHub issues tab.
+
+## License
+
+This project is open-source, and it is available under the [MIT License](LICENSE).
---
+++ index.js
@@ -0,0 +1,104 @@
+#!/usr/bin/env node
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const JSZip = require('jszip');
+const fs = require('fs/promises');
+const path = require('path');
+
+const argv = yargs(hideBin(process.argv))
+    .option('file', {
+        alias: 'f',
+        type: 'string',
+        description: 'Path to the zip file',
+        demandOption: true,
+    })
+    .option('textFile', {
+        alias: 't',
+        type: 'string',
+        description: 'Name of the text file to edit inside the zip file',
+        default: 'options/index.js',
+    })
+    .option('suffix', {
+        alias: 's',
+        type: 'string',
+        description: 'Suffix to append to the original file name if provided',
+    })
+    .option('output', {
+        alias: 'o',
+        type: 'string',
+        description: 'Path to save the edited zip file'
+    }).argv;
+
+function getOutputPath() {
+    if (argv.suffix) {
+        const originalName = path.basename(argv.file, path.extname(argv.file));
+        const dirName = path.dirname(argv.file);
+        return path.join(dirName, `${originalName}${argv.suffix}${path.extname(argv.file)}`);
+    }
+    return argv.output || argv.file; // if output is not explicitly set, default to input file
+}
+
+function editContent(content) {
+    return content
+        .replace(/\.readOnly/g, '.ReadOnly')
+        .replace(/readOnly/g, 'readOnlY')
+        .replace(/\.canEdit=!1/g, '.canEdit=!0');
+}
+
+async function run() {
+    try {
+        const inputFile = argv.file;
+        const ext = path.extname(inputFile).toLowerCase();
+
+        try {
+            await fs.access(inputFile);
+        } catch {
+            console.error(`Error: File ${inputFile} does not exist.`);
+            process.exit(1);
+        }
+
+        const outputPath = getOutputPath();
+
+        if (ext === '.zip') {
+
+            // Read the Zip file
+            const zipBuffer = await fs.readFile(inputFile);
+            const zip = new JSZip();
+            await zip.loadAsync(zipBuffer);
+
+            // Find and Edit the Text File
+            const textFile = zip.file(argv.textFile);
+            if (!textFile) {
+                console.error(`${argv.textFile} does not exist in the zip file.`);
+                console.log('List of files in the zip file:');
+                zip.forEach((relativePath, zipEntry) => {
+                    console.log(relativePath);
+                });
+                return;
+            }
+
+            let content = await textFile.async('string');
+            content = editContent(content);
+            zip.file(argv.textFile, content); // Replace with the edited content
+
+            // Write the modified Zip file back to disk
+            const outputBuffer = await zip.generateAsync({ type: 'nodebuffer' });
+
+            await fs.writeFile(outputPath, outputBuffer);
+
+            console.log(`Modified zip file saved to ${outputPath}`);
+        } else if (ext === '.js') {
+            let content = await fs.readFile(inputFile, 'utf-8');
+            content = editContent(content);
+            await fs.writeFile(outputPath, content, 'utf-8');
+            console.log(`Modified js file saved to ${outputPath}`);
+        } else {
+            console.error('Error: The input file should be a js or a zip file.');
+            process.exit(1);
+        }
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+    }
+}
+
+run();
